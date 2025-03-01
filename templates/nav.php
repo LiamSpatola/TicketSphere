80900209a7eed662186a11c26a9377a803b04162
Liam Spatola <liam.spatola@gmail.com>
2025-03-01 13:52:13 +1100

    Updated the syntax in nav.php

--- templates/nav.php
+++ templates/nav.php
@@ -8,14 +8,18 @@
     <div class="container-fluid">
         <a href="index.php" class="navbar-brand text-light">TicketSphere</a>
         <ul class="navbar-nav">
-                <?php
-                    if (!isset($_SESSION["userID"])) {
-                        echo('<li class="navbar-item"><a href="login.php" class="nav-link text-light">Login</a></li>');
-                        echo('<li class="navbar-item"><a href="register.php" class="nav-link text-light">Register</a></li>');
-                    } else {
-                        echo('<li class="navbar-item"><a href="logout.php" class="nav-link text-light">Logout</a></li>');
-                    }
-                ?>
+                <?php if (!isset($_SESSION["userID"])): ?>
+                    <li class="navbar-item">
+                        <a href="login.php" class="nav-link text-light">Login</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="register.php" class="nav-link text-light">Register</a>
+                    </li>
+                <?php else: ?>
+                    <li class="navbar-item">
+                        <a href="logout.php" class="nav-link text-light">Logout</a>
+                    </li>
+                <?php endif; ?>
         </ul>
     </div>
 </nav>
