e65da3c52017003dc2f0d3fcd77b093a0706220b
Liam Spatola <liam.spatola@gmail.com>
2025-03-01 11:57:55 +1100

    Created the home page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require("templates/head.php"); ?>
+    <title>TicketSphere - Home</title>
+</head>
+<body>
+    <?php require("templates/nav.php"); ?>
+
+    <h1 class="text-center pt-5"><strong>Welcome.</strong></h1>
+    <p class="text-center pb-5">Welcome to TicketSphere, the home of ticketing!</p>
+
+    <div class="container mt-5">
+    <div class="row align-items-start">
+        <div class="col">
+            <h1 class="text-center"><span class="material-symbols-outlined" style="font-size: 100px;">local_activity</span></h1>
+            <p class="text-center">Easily browse, purchase, and manage tickets for your favorite events. Whether it’s concerts, festivals, or exclusive gatherings, TicketSphere makes the process seamless. Say goodbye to long lines and last-minute hassles—secure your spot in just a few clicks. Your next great experience starts here.</p>
+        </div>
+        <div class="col">
+            <h1 class="text-center"><span class="material-symbols-outlined" style="font-size: 100px;">barcode_scanner</span></h1>
+            <p class="text-center">Enjoy quick and secure entry with digital barcodes. No need to print—simply scan your ticket at the venue for instant access. Our smart system ensures smooth check-ins, reducing wait times and keeping events organized. Safe, fast, and hassle-free.</p>
+        </div>
+        <div class="col">
+            <h1 class="text-center"><span class="material-symbols-outlined" style="font-size: 100px;">book_online</span></h1>
+            <p class="text-center">Take your tickets wherever you go with mobile access. Manage bookings, receive updates, and never worry about losing a paper ticket again. With TicketSphere, your event experience is always at your fingertips. Just open, scan, and enjoy!</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ templates/nav.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-sm bg-primary">
+    <div class="container-fluid">
+        <a href="index.php" class="navbar-brand text-light">TicketSphere</a>
+        <ul class="navbar-nav">
+            <li class="navbar-item">
+                <a href="" class="nav-link text-light"></a>
+            </li>
+        </ul>
+    </div>
+</nav>
