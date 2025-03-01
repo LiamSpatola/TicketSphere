c39eb0d3b094b02b433d131537dde3ae125b7506
Liam Spatola <liam.spatola@gmail.com>
2025-03-01 11:37:30 +1100

    Added a database connection script

---
+++ util/dbConnect.php
@@ -0,0 +1,12 @@
+<?php
+    // Getting the database credentials
+    require("settings.php");
+
+    // Initiating a connection to the database
+    $conn = new mysqli($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_NAME);
+
+    if ($conn->connect_error) {
+        // Handling connection errors
+        die("Failed to connect to the database: ".$conn->connect_error);
+    }
+?>
