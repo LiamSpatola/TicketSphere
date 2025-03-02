621cb19390b6c0ec809a3311ba81ac6bd1cb4f40
Liam Spatola <liam.spatola@gmail.com>
2025-03-02 14:44:20 +1100

    Added scan tickets to the navbar for admin users only

--- templates/nav.php
+++ templates/nav.php
@@ -9,13 +9,15 @@
         <a href="index.php" class="navbar-brand text-light">TicketSphere</a>
         <ul class="navbar-nav">
             <?php if (!isset($_SESSION["userID"])): ?>
+                <!-- Nav bar for users not logged in -->
                 <li class="navbar-item">
                     <a href="login.php" class="nav-link text-light">Login</a>
                 </li>
                 <li class="navbar-item">
                     <a href="register.php" class="nav-link text-light">Register</a>
                 </li>
-            <?php else: ?>
+            <?php elseif (isset($_SESSION["userID"]) && !$_SESSION["isAdmin"]): ?>
+                <!-- Nav bar for non-admin logged in users -->
                 <li class="navbar-item">
                     <a href="myTickets.php" class="nav-link text-light">My Tickets</a>
                 </li>
@@ -24,6 +26,14 @@
                 </li>
                 <li class="navbar-item">
                     <a href="cart.php" class="nav-link text-light">Cart</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="logout.php" class="nav-link text-light">Logout</a>
+                </li>
+            <?php else: ?>
+                <!-- Nav bar for admin users -->
+                <li class="navbar-item">
+                    <a href="scanTickets.php" class="nav-link text-light">Scan Tickets</a>
                 </li>
                 <li class="navbar-item">
                     <a href="logout.php" class="nav-link text-light">Logout</a>
